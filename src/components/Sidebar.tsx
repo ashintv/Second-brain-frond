c8a9f753874e84f259ce5a620ff48f3bea497a22
ASHIN T V <146656061+ashintv@users.noreply.github.com>
2025-06-04 00:19:40 +0530

    remeved md

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,7 +2,7 @@ import {  LottusIcon, TwitterIcon, YouTubeIcon } from "./icos"
 import { SidebarItem } from "./SidebarItem"
 
 
-const md = 'text-2xl flex gap-2 text-purple-800 pt-2 font-bold font-serif'
+
 
 export const Sidebar = () => {
         return <div className="min-h-screen min-w-72  pl-6 fixed">
